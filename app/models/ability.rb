35be2e417ad1550973bc2e2bac866e09056b7728
Ekpenisi Erue Raphael <ekpenisiraphael@gmail.com>
2023-03-01 14:36:09 +0530

    Authorizations implemented for all Current_user

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,7 +3,14 @@
 class Ability
   include CanCan::Ability
 
-  def initialize(user)
+  def initialize(current_user)
+    return unless current_user.present?
+
+    can :manage, Category, user: current_user
+    can :manage, Expense, user: current_user
+    can :manage, CategoryExpense do |category_expense|
+      category_expense.category.user == current_user
+    end
     
   end
 end
